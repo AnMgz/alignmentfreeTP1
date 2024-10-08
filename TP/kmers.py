2dd7a6313effa33a6e5245e359750dad188ec17e
root <root@DESKTOP-N4USO8I>
2024-10-08 12:28:34 +0200

    inverse complement bon, pls seq à implementer

--- TP/kmers.py
+++ TP/kmers.py
@@ -16,34 +16,42 @@ def kmer2str(val, k):
 
 def encode_kmer(text, k):
     kmer = 0
-    kmer_inv = 0
     for letter in text[0:k]:
-        kmer_inv<<=2 
         kmer<<=2
         kmer = kmer + encode_nucl(letter)
-        kmer_inv = kmer_inv + rev_nuc(encode_nucl(letter))
-    return kmer, kmer_inv
+    return kmer
 
 def stream_kmers(text, k):
     """enumerate_kmer"""
     text = text[0]
     mask = (1<<(2*(k-1)))-1
-    kmer, kmer_inv = encode_kmer(text,k)
+    kmer = encode_kmer(text,k)
+    kmer_inv = encode_kmer_rev(kmer,k)
     for i in range(len(text)-(k)):
         yield min(kmer, kmer_inv)
         kmer &= mask
-        kmer_inv &= mask
         kmer <<= 2
-        kmer_inv <<= 2
+        kmer_inv >>= 2
         kmer = kmer + encode_nucl(text[i+k])
-        kmer_inv = kmer_inv + rev_nuc(encode_nucl(text[i+k]))
+        kmer_inv = (encode_nucl(rev_nuc(text[i+k]))<<(2*(k-1))) + kmer_inv
     yield min(kmer, kmer_inv)
 
 def encode_nucl(letter):
-    encoding = {"A" : 0, "C" : 1, "T" : 2, "G" : 3}
-    return encoding[letter]
+    return {"A" : 0, "C" : 1, "T" : 2, "G" : 3}[letter]
 
+"""
 def rev_nuc(n_int) :
     return (n_int + 2) & 3
+    """
 
+def rev_nuc(n) :
+    return {"A" : "T", "G": "C", "T": "A" , "C":"G"}[n]
 
+def encode_kmer_rev(kmer,k) : 
+    kmer_inv = 0
+    text = kmer2str(kmer,k)
+    text_rev = text[::-1]
+    for letter in text_rev:
+        kmer_inv<<=2
+        kmer_inv = kmer_inv + encode_nucl(rev_nuc(letter))
+    return kmer_inv
